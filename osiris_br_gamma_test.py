025653554925f1f915d2b4626e4984311ac5d55a
dchu808 <dchu@astro.ucla.edu>
2016-09-07 16:31:58 -0700

    now the program successfully loops through the given files and plots the sums

--- osiris_br_gamma_test.py
+++ osiris_br_gamma_test.py
@@ -36,48 +36,77 @@ def test(width = 5, start = 175, finish = 190):
     # data_cube_file = fits.open('test_data/'+files_list[0])
     # data_cube = data_cube_file[0].data
     
-    data_cube, header = pf.getdata('test_data/'+files_list[0], header=True)
-    noise = pf.getdata('test_data/'+files_list[0], ext=1)
-    quality = pf.getdata('test_data/'+files_list[0], ext=2)
+    # data_cube, header = pf.getdata('test_data/'+files_list[0], header=True)
+    # noise = pf.getdata('test_data/'+files_list[0], ext=1)
+    # quality = pf.getdata('test_data/'+files_list[0], ext=2)
+    
+    #print len(files_list)
+    
+    ##make an array of the pixels, centered around the center pixel
+    box_width = (width - 1)/2
+    
+    ##loop through all the files to run the calculation
+    for i in range(len(files_list)):
+        data_cube, header = pf.getdata('test_data/'+files_list[i], header=True)
+        noise = pf.getdata('test_data/'+files_list[i], ext=1)
+        quality = pf.getdata('test_data/'+files_list[i], ext=2)
+        
+        ##make an array of x-pixels, centered around the center x-pixel, with a width given above
+        x_array = np.arange(centers_array[i][0] - box_width,centers_array[i][0] + box_width + 1,step = 1)
+        ##make an array of y-pixels, centered around the center y-pixel, with a width given above
+        y_array = np.arange(centers_array[i][1] - box_width,centers_array[i][1] + box_width + 1,step = 1)
+        
+        ##make a smaller data cube, only including those pixels and spectral channels specified above
+        ##make a cut only included given x-pixels, y-pixels, and spectral channels
+        new_data_cube = data_cube[centers_array[i][0] - box_width:centers_array[i][0] + box_width + 1,
+            centers_array[i][1] - box_width:centers_array[i][1] + box_width + 1,
+            start:finish+1]
+            
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1,1,1)
+        ax1.imshow(np.sum(new_data_cube, axis=2), cmap="hot", origin="lower")
+        #norm=LogNorm()
+        print np.sum(new_data_cube, axis=2)
+        plt.show()
     
     
     # print data_cube
-    print data_cube.shape[0]
-    print data_cube.shape[1]
+    # print data_cube.shape[0]
+    # print data_cube.shape[1]
     # print data_cube.shape[2]
     ##format is [x_pixel,y_pixel,spectral channel]
     # print data_cube[centers_array[0][0],centers_array[0][1],181]
     
     ##make an array of the pixels, centered around the center pixel
-    box_width = (width - 1)/2
+    # box_width = (width - 1)/2
     # print box_width
     ##make an array of x-pixels, centered around the center x-pixel, with a width given above
-    x_array = np.arange(centers_array[0][0] - box_width,centers_array[0][0] + box_width + 1,step = 1)
+    # x_array = np.arange(centers_array[0][0] - box_width,centers_array[0][0] + box_width + 1,step = 1)
     ##test
     # x_array = np.arange(20 - box_width,20 + box_width + 1,step = 1)
     # print centers_array[0][0]
-    print x_array
+    # print x_array
     
     ##make an array of y-pixels, centered around the center y-pixel, with a width given above
-    y_array = np.arange(centers_array[0][1] - box_width,centers_array[0][1] + box_width + 1,step = 1)
+    # y_array = np.arange(centers_array[0][1] - box_width,centers_array[0][1] + box_width + 1,step = 1)
     ##test
     # y_array = np.arange(35 - box_width,35 + box_width + 1,step = 1)
     # print centers_array[0][1]
-    print y_array
+    # print y_array
     
     #make a smaller data cube, only including those pixels and spectral channels specified above
-    new_data_cube = data_cube[centers_array[0][0] - box_width:centers_array[0][0] + box_width + 1,
-        centers_array[0][1] - box_width:centers_array[0][1] + box_width + 1,
-        start:finish+1]
+    # new_data_cube = data_cube[centers_array[0][0] - box_width:centers_array[0][0] + box_width + 1,
+    #     centers_array[0][1] - box_width:centers_array[0][1] + box_width + 1,
+    #     start:finish+1]
     
     ##test    
     # new_data_cube = data_cube[20 - box_width:20 + box_width + 1,
     #     35 - box_width:35 + box_width + 1,
     #     start:finish+1]
         
-    print new_data_cube.shape[0]
-    print new_data_cube.shape[1]
-    print new_data_cube.shape[2]
+    # print new_data_cube.shape[0]
+    # print new_data_cube.shape[1]
+    # print new_data_cube.shape[2]
     
     ##make a meshgrid for these pixel positions
     # (xi,yi) = np.meshgrid(x_array,y_array)
