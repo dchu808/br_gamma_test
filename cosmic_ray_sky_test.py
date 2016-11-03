e75f6252a69fc65fd317bbad0d43aca928bae6ec
dchu808 <dchu@astro.ucla.edu>
2016-11-02 17:49:50 -0700

    plots the rest of things needed

--- cosmic_ray_sky_test.py
+++ cosmic_ray_sky_test.py
@@ -18,9 +18,9 @@ def plots(file_name):
     ##this file did not have clean cosmic ray
     no_cr_data, header = pf.getdata('/u/devinchu/OSIRIS/cosmic_ray_sky_test/cr_skies/no_cosmic_ray/'+file_name, header=True)
     # noise = pf.getdata('/u/devinchu/OSIRIS/cosmic_ray_sky_test/cr_skies/old_cosmic_ray/'+file_name, ext=1)
-    no_cr_quality = pf.getdata('/u/devinchu/OSIRIS/cosmic_ray_sky_test/cr_skies/old_cosmic_ray/'+file_name, ext=2)
+    no_cr_quality = pf.getdata('/u/devinchu/OSIRIS/cosmic_ray_sky_test/cr_skies/no_cosmic_ray/'+file_name, ext=2)
     
-    # print no_cr_data[:,:,10]
+    print no_cr_quality
     
     ##old cosmic ray
     ##uses clean cosmic ray from develop branch
@@ -33,16 +33,26 @@ def plots(file_name):
     new_cr_quality = pf.getdata('/u/devinchu/OSIRIS/cosmic_ray_sky_test/cr_skies/new_cosmic_ray/'+file_name, ext=2)
     
     ##plot figure
+    ##plot a pretty empty channel, with cosmic rays
     fig = plt.figure(figsize = (10,16))
 
     ax1 = fig.add_subplot(2,3,1)
-    cax = ax1.imshow(no_cr_data[:,:,10], cmap="hot", origin="lower",interpolation="nearest")
+    cax = ax1.imshow(no_cr_data[:,:,5], cmap="hot", origin="lower",interpolation="nearest")
     plt.title(file_name)
     
     ax2 = fig.add_subplot(2,3,2)
-    cax = ax2.imshow(old_cr_data[:,:,10], cmap="hot", origin="lower",interpolation="nearest")
+    cax = ax2.imshow(old_cr_data[:,:,5], cmap="hot", origin="lower",interpolation="nearest")
     
     ax3 = fig.add_subplot(2,3,3)
-    cax = ax3.imshow(new_cr_data[:,:,10], cmap="hot", origin="lower",interpolation="nearest")
+    cax = ax3.imshow(new_cr_data[:,:,5], cmap="hot", origin="lower",interpolation="nearest")
+    
+    ax4 = fig.add_subplot(2,3,4)
+    cax = ax4.imshow(no_cr_data[:,:,153], cmap="hot", origin="lower",interpolation="nearest")
+    
+    ax5 = fig.add_subplot(2,3,5)
+    cax = ax5.imshow(old_cr_data[:,:,153], cmap="hot", origin="lower",interpolation="nearest")
+    
+    ax6 = fig.add_subplot(2,3,6)
+    cax = ax6.imshow(new_cr_data[:,:,153], cmap="hot", origin="lower",interpolation="nearest")
     
     plt.show()
